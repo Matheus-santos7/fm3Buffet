29ed83b8dbbe18e32c1089c1de1357a75fd9cd38
Matheus-santos7 <matheussantosgc@icloud.com>
2024-09-09 22:26:56 -0300

    feat: news configs

--- server/config.js
+++ server/config.js
@@ -1,23 +1,22 @@
+require('dotenv').config();
 
-var config = {
-    dev: {
-        url: 'http://localhost',
-        port: 3000,
-        ambiente: 'DEV',
+const config = {
+    prod: {
+        url: process.env.APP_DOMINIO || process.env.APP_IP || 'http://localhost',
+        port: process.env.APP_PORT || 3000,
+        ambiente: process.env.APP_AMBIENTE,
         database: {
-            host: '92.113.39.169',
-            port: 3306,
-            user: 'admin',
-            password: '1101M@theuss',
-            database: 'deliveryOnline'
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
         },
     }
 }
 
 exports.get = function get(ambiente) {
-
-    if (ambiente.toLowerCase() === 'dev') {
-        return config.dev
+    if (ambiente.toLowerCase() === 'prod') {
+        return config.prod;
     }
-
 }
